1ac30ab874ee0eb35368e8007ab0e16ba42e49b0
Malshi Wanigasingha <139845170+malshiwanigasinha@users.noreply.github.com>
2024-05-28 16:35:15 +0530

    new

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink,RouterLinkActive } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.css'
 })
