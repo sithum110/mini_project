5b994d761d0c1e100147267f26d85418892ad96e
Sithum Wijesinghe <sithumwijesinghe59@gmail.com>
2024-08-18 23:22:36 +0530

    add roomate matching AI part into back end

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/user.routes');
 const forumRoutes = require('./routes/forum.routes');
 const contactRoutes = require('./routes/contact.routes');
+const accommodationModel = require('./models/accommodation.model');
+const recommendationRoutes = require('./routes/recommendation.routes');
+const accommodationRoutes = require('./routes/accommodation.routes');
 
 const app = express();
 const PORT = 3000;
@@ -22,6 +25,8 @@ mongoose.connect('mongodb://localhost:27017/colive', {
 app.use('/api', userRoutes);
 app.use('/api', forumRoutes);
 app.use('/api', contactRoutes);
+app.use('/api/recommendations', recommendationRoutes);
+app.use('/api/accommodations', accommodationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
