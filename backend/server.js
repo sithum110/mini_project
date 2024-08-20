1be77ed8dbcde6c397af5c8e0325881c9d4ee0ee
Kalpana_Perera <118042669+KalpanaPerera@users.noreply.github.com>
2024-08-20 14:09:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const forumRoutes = require('./routes/forum.routes');
 const contactRoutes = require('./routes/contact.routes');
 const detailRoutes = require('./routes/detail.routes');
 const propertyRoutes = require('./routes/property.routes');
-
+const listRoutes = require('./routes/list.routes');
 
 const app = express();
 const PORT = 3000;
@@ -15,22 +15,18 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/signup_DB', {
-
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/colive', 
+mongoose.connect('mongodb://localhost:27017/colive', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
-})
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.error('Connection error:', err));
+});
+
 
 app.use('/api', userRoutes);
 app.use('/api', forumRoutes);
 app.use('/api', contactRoutes);
 app.use('/api', detailRoutes);
 app.use('/api', propertyRoutes);
+app.use('/api', listRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
